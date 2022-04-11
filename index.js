e745da3e4f5341705ca95f77fb0b5260797c120f
azreelteal <azreelro.khanbo@gmail.com>
2022-04-11 21:26:23 +0530

    server

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-const express = require('express');
+import express from 'express'
 const app = express();
-const http = require('http');
+import http from 'http'
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+import { Server } from "socket.io";
 const io = new Server(server);
 const port = process.env.PORT || 3001
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDoc, doc, updateDoc } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDu2afD9seUM-Cigy0WHhEVjgGPcqMoUZI",
+  authDomain: "mathque-7c80e.firebaseapp.com",
+  databaseURL: "https://mathque-7c80e-default-rtdb.firebaseio.com",
+  projectId: "mathque-7c80e",
+  storageBucket: "mathque-7c80e.appspot.com",
+  messagingSenderId: "576042213006",
+  appId: "1:576042213006:web:7253371bb48cf3f5f8b3df"
+};
+
+
+const app2 = initializeApp(firebaseConfig);
+const db = getFirestore(app2);
+
+const snap = await doc(db, 'Users', '101411107148464225590');
+await updateDoc(snap, {
+  userCoins: 82
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-let roomName = 0;
+
+
+
+
 let users = new Map();
 let usersInRoom = [];
 
@@ -24,6 +49,7 @@ io.on('connection', (socket) => {
 
         let socket1 = usersInRoom[0].socket;
         let socket2 = usersInRoom[1].socket;
+        let roomName = createUuid();
 
         socket1.join(roomName);
         socket2.join(roomName);
@@ -37,21 +63,40 @@ io.on('connection', (socket) => {
         socket2.emit('questions', { questions: listofquestions} );
 
         usersInRoom = [];
-        roomName++;
   
     } 
-    else { 
+    else { socket.emit("message", "waiting for other user"); } });
 
-        socket.emit("message", "waiting for other user"); }} );
+    socket.on('message_other', (msg, room) => { io.to(room).emit("message", msg); });
 
-        socket.on('message_other', (msg, room) => { io.to(room).emit("message", msg); });
+    socket.on("disconnecting", (reason) => { console.log(`reason: ${reason}`);
 
-        socket.on('disconnect', () => { console.log(`disconnected`); });
+            let w = 0;
+            for (let i of users.keys()){
+                //console.log(i);
+                let userr = Array.from(users.get(i));
+                if (socket.id === userr[0].id){
+                    console.log(`user left with users id : ${userr[0].id}`);
+                    io.to(userr[1].id).emit('message', "other user disconnected");
+                    users.delete(i);
+                    w = 1;
+                    break;
+                }else{
+                    console.log(`user left with users id : ${userr[1].id}`);
+                    io.to(userr[0].id).emit('message', "other user disconnected");
+                    users.delete(i);
+                    w = 1;
+                    break;
+                }
+            }
 
-    socket.on('remove_user', (room)=>{  users.delete(room); });
+            if (w === 0){
+                usersInRoom = [];
+            }
+        
+     });
 
-    socket.on('disconnect', ()=>{ console.log(`user disconnected`); })
-
+    socket.on('disconnect', ()=>{ console.log(`user disconnected`); });
   
 });
 
@@ -59,8 +104,8 @@ server.listen(port, () => {console.log('listening on *:', port);});
 
 
 //<-------------------------------------Functions are defined here--------------------------------------->
-isEnoughUsers = () => usersInRoom.length === 2;
-generateEasyQuestions = () => {let questions = {}; 
+function isEnoughUsers() { usersInRoom.length === 2; }
+function generateEasyQuestions() {let questions = {}; 
     for (let sno = 1; sno <= 5; sno++){
         let num1 = Math.floor((Math.random() * 100) + 1);
         let num2 = Math.floor((Math.random() * 100) + 1);
@@ -76,3 +121,15 @@ generateEasyQuestions = () => {let questions = {};
             questions[sno.toString()] = question;
         }
         return questions;}
+function removeUser(room) { users.delete(room);}
+function createUuid() { var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid; }
+
+
+
+//<---------------------------------Garbage--------------------------------->//
