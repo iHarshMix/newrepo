af34402f246bcc3e763c7b2d621e0bbf824a37c4
azreelteal <azreelro.khanbo@gmail.com>
2022-04-17 13:32:07 +0530

    update

--- index.js
+++ index.js
@@ -140,11 +140,11 @@ io.on('connection', (socket) => {
 
     });
 
-
+        //console.log what the hell is wrong 
     socket.on('get_result', (room) =>{
         if (userResult.has(room.room)){
             let old = Array.from(userResult.get(room.room));
-            console.log(old[0].report);
+            //console.log(old[0].report);
             //console.log(old[1].report);
             socket.emit("user_results", {"firstReport" : old[0].report});
             
