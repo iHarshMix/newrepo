885be3e0d97a6bab5a3128e99a63e89135322430
azreelteal <azreelro.khanbo@gmail.com>
2022-07-30 13:17:53 +0530

    update

--- index.js
+++ index.js
@@ -149,8 +149,8 @@ io.on('connection', (socket) => {
                 "report" : reportString,
             };
 
-            old.socket.emit('userReport', {"user1" : JSON.stringify(user1), "user2": JSON.stringify(user2)});
-            socket.emit('userReport', {"user1" : JSON.stringify(user1), "user2": JSON.stringify(user2)});
+            old.socket.emit('userReport', {"user1" : user1, "user2": user2});
+            socket.emit('userReport', {"user1" : user1, "user2": user2});
             //old.socket.e('userReport', {"user1" : JSON.stringify(user1), "user2": JSON.stringify(user2)});
 
             userResult.delete(room);
